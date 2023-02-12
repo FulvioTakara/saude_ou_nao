24a2de97e26b63e1b801e98f8b5977af6c93b027
unknown <fulviojp@gmail.com>
2023-02-12 08:23:30 -0300

    Foi adicionado documentação

--- saude.py
+++ saude.py
@@ -1,8 +1,10 @@
 """Estudo prático sobre classes e objetos"""
+# Foi criada a classe de referência
 class Corpo():
     _forte = False;
     _obeso = False;
 
+# Foram criadas as classes herdeiras
 class Adoecido(Corpo):
     _obeso = True;
     print('Cuidado: Ficar acima do peso pode gerar patologias como diabetes, colesterol, hipertensão.')
@@ -11,6 +13,7 @@ class Saudavel(Corpo):
     _forte = True;
     print("Parabéns: Um percentual alto de musculos evita patologias como diabetes, colesterol, hipertensão.")
 
+# Objetos
 Maria = Adoecido;
 print(Maria);
 Almerinda = Saudavel;
