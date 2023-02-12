07674f142b1936fb4d7dd8048542df6e5335dde3
unknown <fulviojp@gmail.com>
2023-02-12 08:19:21 -0300

    Classes e objetos criados

--- saude.py
+++ saude.py
@@ -1,2 +1,19 @@
 """Estudo prático sobre classes e objetos"""
+class Corpo():
+    _forte = False;
+    _obeso = False;
 
+class Adoecido(Corpo):
+    _obeso = True;
+    print('Cuidado: Ficar acima do peso pode gerar patologias como diabetes, colesterol, hipertensão.')
+
+class Saudavel(Corpo):
+    _forte = True;
+    print("Parabéns: Um percentual alto de musculos evita patologias como diabetes, colesterol, hipertensão.")
+
+Maria = Adoecido;
+print(Maria);
+Almerinda = Saudavel;
+print(Almerinda);
+Joao = Adoecido;
+print(Joao);
